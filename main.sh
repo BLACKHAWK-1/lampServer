c25335b54b33ead3209f427c20179128b25c324a
Arisa Onwuka <onwuka.arisa@gmail.com>
2023-05-20 02:44:40 +0100

    added the main driver program that calls up the sub-modules. the main driver makes the sub-modules executable by using the chmod function

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#this is the entry point of the program
+
+#the various sub modules are made executable using chmod
+echo "granting executable permissions to the sub-modules "update.sh, apache2.sh,mysql.sh and php.sh"....."
+chmod +x ./update.sh ./apache2.sh ./mysql.sh ./php.sh
+
+
+#the various sub-modules are called up
+./update.sh
+./apache2.sh
+./mysql.sh
+./php.sh
--- update.sh
+++ update.sh
@@ -5,5 +5,3 @@ echo
 echo
 echo "Update Successfully Done"
 echo
-echo "granting executable permissions to the sub-modules "apache2.sh,mysql.sh and php.sh"....."
-chmod +x ./apache2.sh mysql.sh php.sh
